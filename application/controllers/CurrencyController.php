4ba6910cbfc7f94b3f6b22ef907550e163471ba7
EriksOsis <osiseriks2@gmail.com>
2022-05-19 23:20:43 +0300

    Cleaning code

--- application/controllers/CurrencyController.php
+++ application/controllers/CurrencyController.php
@@ -14,50 +14,55 @@ class CurrencyController extends CI_Controller
 	public function convertCurrency($amount, $from_currency, $to_currency): string
 	{
 		$total = 0;
-		if ($amount && $from_currency && $to_currency) {
-			if (!gettype($this->string) == gettype($from_currency) && !gettype($this->string) == gettype($to_currency)) {
-				$apikey = '400df89c9ea465ac6491';
-
-				$from_Currency = urlencode(strtoupper($from_currency));
-				$to_Currency = urlencode(strtoupper($to_currency));
-				$query = "{$from_Currency}_{$to_Currency}";
-
-				$json = file_get_contents("https://free.currconv.com/api/v7/convert?q={$query}&compact=ultra&apiKey={$apikey}");
-
-				$obj = json_decode($json, true);
-
-				$val = floatval($obj["$query"]);
-
-				$total += $val * $amount;
+		if (!$amount && !$from_currency && !$to_currency) {
+			if (gettype($this->string) !== gettype($from_currency) || gettype($this->string) !== gettype($to_currency)) {
+				return number_format($total, 2, '.', '');
 			}
 		}
+		$apikey = '400df89c9ea465ac6491';
+
+		$from_Currency = urlencode(strtoupper($from_currency));
+		$to_Currency = urlencode(strtoupper($to_currency));
+		$query = "{$from_Currency}_{$to_Currency}";
+
+		$json = file_get_contents("https://free.currconv.com/api/v7/convert?q={$query}&compact=ultra&apiKey={$apikey}");
+
+		$obj = json_decode($json, true);
+
+		$val = floatval($obj["$query"]);
+
+		$total += $val * $amount;
+
 		return number_format($total, 2, '.', '');
+
 	}
 
 	//takes 3 parameters and returns currency pairs value on a certain date in the past
 	public function show($from_currency, $to_currency, $date): float|string
 	{
 
-		if ($from_currency && $to_currency && $date) {
-			if (!gettype($this->string) == gettype($from_currency) && !gettype($this->string) == gettype($to_currency)) {
-				$apikey = '400df89c9ea465ac6491';
-
-				$from_Currency = urlencode($from_currency);
-				$to_Currency = urlencode($to_currency);
-				$query = "{$from_Currency}_{$to_Currency}";
-
-				$json = file_get_contents("https://free.currconv.com/api/v7/convert?q={$query}&compact=ultra&date={$date}&apiKey={$apikey}");
-
-				$obj = json_decode($json, true);
-
-				foreach ($obj as $value) {
-					foreach ($value as $key) {
-						return number_format($key, 2, '.', '');
-					}
-				}
+		if (!$from_currency && !$to_currency && !$date) {
+			if (gettype($this->string) !== gettype($from_currency) && gettype($this->string) !== gettype($to_currency)) {
+				return "Enter values";
 			}
 		}
-		return "Enter values";
+
+		$apikey = '400df89c9ea465ac6491';
+
+		$from_Currency = urlencode($from_currency);
+		$to_Currency = urlencode($to_currency);
+		$query = "{$from_Currency}_{$to_Currency}";
+
+		$json = file_get_contents("https://free.currconv.com/api/v7/convert?q={$query}&compact=ultra&date={$date}&apiKey={$apikey}");
+
+		$obj = json_decode($json, true);
+
+		foreach ($obj as $value) {
+			foreach ($value as $key) {
+				return number_format($key, 2, '.', '');
+			}
+		}
+		return 0.00;
 	}
 }
 
